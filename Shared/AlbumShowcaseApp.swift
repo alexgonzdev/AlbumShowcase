6b05e10057c9c0207be2b0a36d545eb2c3ea0126
Alex Gonzalez <alexgonzdev@gmail.com>
2022-01-19 12:15:14 -0400

    Initial Commit

---
+++ Shared/AlbumShowcaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlbumShowcaseApp.swift
+//  Shared
+//
+//  Created by Alex Gonzalez on 1/19/22.
+//
+
+import SwiftUI
+
+@main
+struct AlbumShowcaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
